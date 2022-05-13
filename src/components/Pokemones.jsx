1e6d258746e3e5d72fdaa54e6e3ba95ab3d3c48e
Pablo Gallardo <juanito.gallardo_26@hotmail.com>
2022-05-13 18:06:26 -0500

    Pokemones dinamicos con useEffect

--- src/components/Pokemones.jsx
+++ src/components/Pokemones.jsx
@@ -3,24 +3,33 @@ import React, { useState, useEffect } from 'react'
 const Pokemones = () => {
     
     const [pokemones, setPokemones] = useState([]);
+    const [anterior, setAnterior] = useState(null);
+    const [siguiente,setSiguiente] = useState(null);
+    const [actual, setActual] = useState('https://pokeapi.co/api/v2/pokemon?limit=10&offset=0');
 
     useEffect(() => {
         async function obtenerPokemones() {
-            const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=10&offset=0');
+            const response = await fetch(actual);
             const data = await response.json();
-
+            console.log(data);
             setPokemones(data.results);
+            setAnterior(data.previous);
+            setSiguiente(data.next);
         }
 
         obtenerPokemones();
-    }, [])
+    }, [actual])
 
     return (
-        <ul>        
-            {pokemones.map((pokemon, index) => {
-                return <li key={index}>{pokemon.name}</li>
-            })}
-        </ul>
+        <div>
+            <ul>        
+                {pokemones.map((pokemon, index) => {
+                    return <li key={index}>{pokemon.name}</li>
+                })}
+            </ul>
+            <button onClick={() => anterior !== null && setActual(anterior)}>Anteriores</button>
+            <button onClick={() => siguiente !== null && setActual(siguiente)}>Siguientes</button>
+        </div>
     )
 }
 
