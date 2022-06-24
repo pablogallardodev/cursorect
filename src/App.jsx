e53ffc9a5379f490db182b710815d1681e239c03
Pablo Gallardo <juanito.gallardo_26@hotmail.com>
2022-06-24 17:41:53 -0500

    React 17: Add bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,22 @@ import ListaNombres from "./components/ListaNombres";
 import Pokemones from "./components/Pokemones";
 import Referencia from "./components/Referencia";
 import Estilos from "./components/Estilos"
+import Navbar from "./components/Navbar";
 
 import "./App.css"
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
 
   return <div className="container">
-    {/* <Texto name="Pablo" apellido="Gallardo" edad={25} />
+    <Texto name="Pablo" apellido="Gallardo" edad={25} />
     <Texto name="Juan" apellido="Ochoa" edad={30}/> 
     <Contador />
     <ListaNombres />
     <Pokemones />
-    <Referencia /> */}
+    <Referencia />
     <Estilos />
+    <Navbar />
   </div>
 }
 
