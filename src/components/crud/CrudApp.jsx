17d1757008aceae524137429c7e333974c7d0c00
Pablo Gallardo <juanito.gallardo_26@hotmail.com>
2022-07-09 15:55:16 -0500

    React 19: CRUD: iserció datos

--- src/components/crud/CrudApp.jsx
+++ src/components/crud/CrudApp.jsx
@@ -3,22 +3,28 @@ import CrudForm from './CrudForm'
 import CrudTable from './CrudTable'
 
 const baseDatos = [
-  { id:0, equipo:'Barcelona', pais: 'España' },
-  { id:1, equipo:'Guadalajara', pais: 'México' },
-  { id:2, equipo:'Boca JR', pais: 'Argentina' },
-  { id:3, equipo:'Manchester City', pais: 'Inglaterra' },
-  { id:4, equipo:'Real Madrid', pais: 'España' },
+  { id:1, equipo:'Barcelona', pais: 'España' },
+  { id:2, equipo:'Guadalajara', pais: 'México' },
+  { id:3, equipo:'Boca JR', pais: 'Argentina' },
+  { id:4, equipo:'Manchester City', pais: 'Inglaterra' },
+  { id:5, equipo:'Real Madrid', pais: 'España' },
 ]
 
 const CrudApp = () => {
 
   const [equipos, setEquipos] = useState(baseDatos);
 
+  // inserción de datos
+  const addEquipo = (equipo) => {
+    setEquipos([
+      ...equipos,
+      equipo
+    ])
+  }
+
   return <>
     <h2>CRUD de equipos de futbol...</h2>
-
-    <CrudForm />
-
+    <CrudForm addEquipo={addEquipo}/>
     <CrudTable equipos={equipos}/>
   </>
 }
--- src/components/crud/CrudForm.jsx
+++ src/components/crud/CrudForm.jsx
@@ -1,12 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const CrudForm = () => {
+const CrudForm = ({ addEquipo }) => {
+
+  const [formData, setFormData] = useState({
+    equipo: '',
+    pais: '',
+    id: null
+  })
+
+  const handleSubmit = (e) => {
+    e.preventDefault(); // Evitar que se recarge la página
+    
+    if (formData.equipo !== '' && formData.pais !== '') {
+      formData.id = Math.floor(Math.random() * 100)
+      addEquipo(formData)
+      setFormData({
+        equipo: '',
+        pais: '',
+        id: null
+      })
+    } else {
+      alert("Por favor agrega un equipo y pais.")
+    }
+  }
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    })
+  }
+
   return <>
-    <form className='m-3'>
+    <form className='m-3' onSubmit={handleSubmit}>
       <label htmlFor="equipo">Equipo:</label>
-      <input type="text" name="equipo"/>
+      <input type="text" name="equipo" onChange={handleChange} value={formData.equipo}/>
       <label htmlFor="pais">Pais:</label>
-      <input type="text" name="pais"/>
+      <input type="text" name="pais" onChange={handleChange} value={formData.pais}/>
       <input className='btn btn-success mx-1' type="submit" value="Agregar" />
       <input className='btn btn-danger mx-1' type="reset" value="Cancelar" />
     </form>
