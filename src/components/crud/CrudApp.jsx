7729a9dedf16c1fe9656d85bfe00b6b0214525a9
Pablo Gallardo <juanito.gallardo_26@hotmail.com>
2022-07-25 20:44:27 -0500

    React 21: CRUD - eliminar datos

--- src/components/crud/CrudApp.jsx
+++ src/components/crud/CrudApp.jsx
@@ -1,19 +1,22 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import CrudForm from './CrudForm'
 import CrudTable from './CrudTable'
 
-const baseDatos = [
-  { id:1, equipo:'Barcelona', pais: 'España' },
-  { id:2, equipo:'Guadalajara', pais: 'México' },
-  { id:3, equipo:'Boca JR', pais: 'Argentina' },
-  { id:4, equipo:'Manchester City', pais: 'Inglaterra' },
-  { id:5, equipo:'Real Madrid', pais: 'España' },
-]
-
 const CrudApp = () => {
 
-  const [equipos, setEquipos] = useState(baseDatos);
   const [editData, setEditData] = useState(null);
+  const [equipos, setEquipos] = useState(() => {
+    const saveEquipos = window.localStorage.getItem("equiposData");
+    if (saveEquipos) {
+      return JSON.parse(saveEquipos);
+    } else {
+      return []
+    }
+  });
+
+  useEffect(() => {
+    window.localStorage.setItem("equiposData", JSON.stringify(equipos))
+  }, [equipos])
 
   // inserción de datos
   const addEquipo = (equipo) => {
@@ -30,10 +33,20 @@ const CrudApp = () => {
     setEditData(null)
   }
 
+  // Eliminar un equipo
+  const deleteEquipo = id => {
+    const isDelete = window.confirm(`¿Deseas eliminar el registro con id: ${id}?`)
+
+    if (isDelete) {
+      const newEquipos = equipos.filter(el => el.id !== id)
+      setEquipos(newEquipos);
+    }
+  }
+
   return <>
     <h2>CRUD de equipos de futbol...</h2>
     <CrudForm addEquipo={addEquipo} editEquipo={editEquipo} editData={editData}/>
-    <CrudTable equipos={equipos} setEditData={setEditData}/>
+    <CrudTable equipos={equipos} setEditData={setEditData} deleteEquipo={deleteEquipo}/>
   </>
 }
 
--- src/components/crud/CrudTable.jsx
+++ src/components/crud/CrudTable.jsx
@@ -1,33 +1,34 @@
 import React from 'react'
 
-const CrudTable = ({ equipos, setEditData }) => {
-
-  
+const CrudTable = ({ equipos, setEditData, deleteEquipo }) => {
 
   return <>
     <h3>Equipos actuales</h3>
-    <table className='table w-50'>
-      <tr>
-        <td>ID</td>
-        <td>Equipo</td>
-        <td>Pais</td>
-        <td></td>
-      </tr>
+    <table className='table'>
+      <thead>
+        <tr>
+          <td>Equipo</td>
+          <td>Pais</td>
+          <td></td>
+        </tr>
+      </thead>
+      <tbody>
       {
         equipos.length === 0 ? <tr>No hay datos</tr>
         : equipos.map((equipo, index) => {
           return <tr key={index}>
-                  <td>{equipo.id}</td>
                   <td>{equipo.equipo}</td>
                   <td>{equipo.pais}</td>
                   <td>
                     <button className='btn btn-outline-warning mx-1'
                     onClick={() => setEditData(equipo)}>Editar</button>
-                    <button className='btn btn-outline-danger mx-1'>Eliminar</button>
+                    <button className='btn btn-outline-danger mx-1'
+                    onClick={() => deleteEquipo(equipo.id)}>Eliminar</button>
                   </td>
                 </tr>
         })
       }
+      </tbody>
     </table>
   </>
 }
