4f9acf591d9caa74b38988c4a2f1177225a8c53b
Pablo Gallardo <juanito.gallardo_26@hotmail.com>
2022-09-08 19:37:58 -0500

    React 26: Rutas con react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react"
-// import Texto from "./components/Texto"
-// import Contador from "./components/Contador"
-// import ListaNombres from "./components/ListaNombres";
-// import Pokemones from "./components/Pokemones";
-// import Referencia from "./components/Referencia";
-// import Estilos from "./components/Estilos"
-// import Navbar from "./components/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Texto from "./components/Texto"
+import Contador from "./components/Contador"
+import ListaNombres from "./components/ListaNombres";
+import Pokemones from "./components/Pokemones";
+import Referencia from "./components/Referencia";
+import Estilos from "./components/Estilos"
+import Navbar from "./components/Navbar";
 import CrudApp from "./components/crud/CrudApp";
 import CrudAPI from "./components/crud/CrudAPI";
 
@@ -14,18 +15,22 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
 
-  return <div className="container">
-    <CrudAPI />
-    {/* <Texto name="Pablo" apellido="Gallardo" edad={25} />
-    <Texto name="Juan" apellido="Ochoa" edad={30}/> 
-    <Contador />
-    <ListaNombres />
-    <Pokemones />
-    <Referencia />
-    <Estilos />
-    <Navbar /> 
-    <CrudApp />*/}
-  </div>
+  return ( 
+  <div className="container">
+    <BrowserRouter>
+      <Routes>
+        <Route path='/contador' element={<Contador />}/>
+        <Route path='/crudAPI' element={<CrudAPI />}/>
+        <Route path='/crudApp' element={<CrudApp />}/>
+        <Route path='/navbar' element={<Navbar />}/>
+        <Route path='/estilos' element={<Estilos />}/>
+        <Route path='/referencia' element={<Referencia />}/>
+        <Route path='/pokemones' element={<Pokemones />}/>
+        <Route path='/listaNombres' element={<ListaNombres />}/>
+        <Route path='*' element={<h2>Curso React by Pablo Gallardo</h2>}/>
+      </Routes>
+    </BrowserRouter> 
+  </div>)
 }
 
 export default App;
