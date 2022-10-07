066e8b1ea41c08db02bca8ec85328468b3f5713d
Pablo Gallardo <juanito.gallardo_26@hotmail.com>
2022-10-06 19:02:12 -0500

    React 29,30: Validando formulario y enviando información

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -3,29 +3,66 @@ import useForm from "../hooks/useForm"
 const ContactForm = () => {
 
   const initialData = {
-    nombre: 'Pablo',
-    correo: 'correo@prueba.com',
-    asunto: 'Saludo',
-    mensaje: 'Hola soy Pablo Gallardo !!!'
+    nombre: '',
+    correo: '',
+    asunto: '',
+    mensaje: ''
   }
 
-  const { form, loading, handleChange, handleSubmit } = useForm(initialData)
+  const onValidate = (form) => {
+    let errors = {}
+    let regexName = /^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]+$/;
+    let regexEmail = /^(\w+[/./-]?){1,}@[a-z]+[/.]\w{2,}$/;
+    let regexComments = /^.{1,255}$/;
+
+    if (!form.nombre.trim()) {
+      errors.nombre = 'El campo "Nombre" no debe ser vacio.'
+    } else if (!regexName.test(form.nombre)){
+      errors.nombre = 'El campo "Nombre" solo acepta letras y espacios.'
+    }
+
+    if (!form.correo.trim()) {
+      errors.correo = 'El campo "Correo" no debe ser vacio.'
+    } else if (!regexEmail.test(form.correo)){
+      errors.correo = 'El campo "Correo" contiene un formato no valido.'
+    }
+
+    if (!form.asunto.trim()) {
+      errors.asunto = 'El campo "Asunto" no debe ser vacio.'
+    } else if (!regexName.test(form.asunto)){
+      errors.asunto = 'El campo "Asunto" solo acepta letras y espacios.'
+    }
+
+    if (!form.mensaje.trim()) {
+      errors.mensaje = 'El campo "Mensaje" no debe ser vacio.'
+    } else if (!regexComments.test(form.mensaje)){
+      errors.mensaje = 'El campo "Mensaje" acepta solo 255 caracteres.'
+    }
+
+    return errors
+  }
+
+  const { form, errors, loading, handleChange, handleSubmit } = useForm(initialData, onValidate)
 
   return (
-    <form className='w-100'>
+    <form className='w-100' onSubmit={handleSubmit}>
       <label className='form-label'>Nombre</label>
       <input type="text" className='form-control' name="nombre" value={form.nombre} onChange={handleChange}/>
+      {errors.nombre && <div className="alert alert-danger p-1">{errors.nombre}</div>}
 
       <label className='form-label'>Correo electrónico</label>
       <input type="email" className='form-control' name="correo" value={form.correo} onChange={handleChange}/>
+      {errors.correo && <div className="alert alert-danger p-1">{errors.correo}</div>}
 
       <label className='form-label'>Asunto</label>
       <input type="text" className='form-control'name="asunto" value={form.asunto} onChange={handleChange}/>
+      {errors.asunto && <div className="alert alert-danger p-1">{errors.asunto}</div>}
 
       <label className='form-label'>Mensaje</label>
       <textarea className='form-control'name="mensaje" value={form.mensaje} onChange={handleChange}/>
+      {errors.mensaje && <div className="alert alert-danger p-1">{errors.mensaje}</div>}
 
-      <button className='btn btn-warning mt-1 w-100'>Enviar</button>
+      <button className='btn btn-warning mt-1 w-100' disabled={loading}>{loading ? "Enviando..." : "Enviar"}</button>
     </form>
   )
 }
--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,17 +1,44 @@
 import { useState } from 'react'
 
-const useForm = (initialData) => {
-  const [form, setForm] = useState(initialData);
+const useForm = (initialData, onValidate) => {
+  const [form, setForm] = useState(initialData)
   const [loading, setLoading] = useState(false)
+  const [errors, setErros] = useState({})
 
   const handleChange = (event) => {
     const { name, value } = event.target
     setForm({ ...form, [name]: value })
   }
 
-  const handleSubmit = () => { }
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    const err = onValidate(form)
+    setErros(err)
 
-  return { form, loading, handleChange, handleSubmit }
+    if (Object.keys(err).length === 0) {
+      setLoading(true)
+      fetch("https://formsubmit.co/ajax/pablito.260197@gmail.com", {
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        },
+        body: JSON.stringify(form)
+      })
+        .then(response => response.json())
+        .then(data => {
+          console.log(data)
+          data.success === "true" && setForm(initialData)
+          setLoading(false)
+        })
+        .catch(error => {
+          console.log(error)
+          setLoading(false)
+        });
+    }
+  }
+
+  return { form, errors, loading, handleChange, handleSubmit }
 }
 
 export default useForm;
