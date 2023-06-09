056f85cd62c0738531ebe4ae09478508c11e8171
Pablo Gallardo <juanito.gallardo_26@hotmail.com>
2023-06-09 17:59:29 -0600

    React 52: Pokedex - Tema Oscuro

--- 02-pokedex/src/components/Navbar.jsx
+++ 02-pokedex/src/components/Navbar.jsx
@@ -1,7 +1,16 @@
+import { useState, useEffect } from "react";
 import { Logo, Luna, Sol } from "./Icons";
 import './Navbar.css'
 
 const Navbar = () => {
+
+  const [tema, setTema] = useState('claro')
+
+  const handleChange = (e) => setTema(e.target.checked ? 'oscuro' : 'claro')
+
+  useEffect(() => {
+    document.body.setAttribute('data-tema', tema)
+  }, [tema])
 
   return (
     <nav>
@@ -9,7 +18,7 @@ const Navbar = () => {
       <div className="switch">
         <Sol />
         <label>
-          <input type="checkbox" className="check-switch" hidden/>
+          <input type="checkbox" className="check-switch" onChange={handleChange} hidden/>
           <span className="slider"></span>
         </label>
         <Luna />
