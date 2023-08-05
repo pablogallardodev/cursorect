2f76ae0e9894103527338cdf1a075d3f92582953
Pablo Gallardo <juanito.gallardo_26@hotmail.com>
2023-08-04 20:16:56 -0600

    React 57: Calculadora - logica de operaciones y manejo de errores

--- 03-calculadora/src/components/Calculadora.jsx
+++ 03-calculadora/src/components/Calculadora.jsx
@@ -7,10 +7,28 @@ const Calculadora = () => {
   const [data, setData] = useState({ operacion: '', resultado: '' })
 
   const escritura = (event) => {
+    const valor = event.target.innerText
+    const esOperacion = valor === '+' || valor === '-' || valor === '*' || valor === '/' || valor === '%'
 
     if (data.operacion.length >= 10) return
+    if (valor === '+/-' && data.operacion === '') return
+    if (valor === '%' && data.operacion.includes('%')) return
 
-    setData({...data, operacion: `${data.operacion}` + event.target.innerText})
+    if (data.operacion.includes('Error')) {
+      setData({...data, operacion: valor})
+    } else if (data.resultado !== '' && data.operacion === '' && esOperacion) {
+      setData({...data, operacion: `${data.resultado}` + valor})
+    }else if (valor === '+/-' && data.operacion !== '') {
+      
+      if (data.operacion.slice(0,1) === '-') {
+        setData({...data, operacion: `${data.operacion.slice(1, data.operacion.length)}`})
+      } else {
+        setData({...data, operacion: `-${data.operacion}`})
+      }
+
+    } else {
+      setData({...data, operacion: `${data.operacion}` + valor})
+    }    
   }
 
   const eliminar = () => {
@@ -22,10 +40,20 @@ const Calculadora = () => {
   }
 
   const resultado = () => {
+    try {
+      let resultado = ''
 
-    const resultado = eval(data.operacion)
+      if (data.operacion.includes('%')) {
+        const valores = data.operacion.split('%')
+        resultado = eval(`${valores[1]}*(${valores[0]}/100)`)
+      } else {
+        resultado = eval(data.operacion)
+      }
 
-    setData({...data, resultado})
+      setData({...data, resultado, operacion: ''})
+    } catch (error) {
+      setData({...data, operacion: 'Error'})
+    }    
   }
 
   return (
@@ -33,8 +61,8 @@ const Calculadora = () => {
       <span className="resultado">{data.resultado}</span>
       <span className="display">{data.operacion}</span>
       <Boton texto='C' clase='gris' handleClick={limpiar}/>
-      <Boton texto='+/-' clase='gris'/>
-      <Boton texto='%' clase='gris'/>
+      <Boton texto='+/-' clase='gris' handleClick={escritura}/>
+      <Boton texto='%' clase='gris' handleClick={escritura}/>
       <Boton texto='/' clase='operacion' handleClick={escritura}/>
       <Boton texto='7' clase='numero' handleClick={escritura}/>
       <Boton texto='8' clase='numero' handleClick={escritura}/>
@@ -48,7 +76,7 @@ const Calculadora = () => {
       <Boton texto='2' clase='numero' handleClick={escritura}/>
       <Boton texto='3' clase='numero' handleClick={escritura}/>
       <Boton texto='+' clase='operacion' handleClick={escritura}/>
-      <Boton texto='.' clase='numero'/>
+      <Boton texto='.' clase='numero' handleClick={escritura}/>
       <Boton texto='0' clase='numero' handleClick={escritura}/>
       <Boton texto='◀' clase='numero' handleClick={eliminar}/>
       <Boton texto='=' clase='operacion' handleClick={resultado}/>
