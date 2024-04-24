bfaf58cb4b15246a4ed96683b1ecf2c7e0ff062a
pablogallardodev <pablito.260197@gmail.com>
2024-04-23 21:09:10 -0600

    Separacion de proyectos a repos independientes, manejo de codigo

--- 02-pokedex/src/components/Cargando.jsx
+++
@@ -1,5 +0,0 @@
-import './cargando.css'
-
-const Cargando = () => <div className="cargando"></div>
-
-export default Cargando
--- 03-calculadora/src/components/Boton.jsx
+++
@@ -1,9 +0,0 @@
-import './boton.css'
-
-const Boton = (params) => {
-  const {texto, clase, handleClick} = params
-  
-  return <button className={clase} onClick={handleClick}>{texto}</button>
-}
-
-export default Boton
